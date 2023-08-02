27e0434ad3cc40eade9c476878fd9edc652e9ba9
JuflyZ0r <danil.suslov.85@gmail.com>
2023-08-02 15:05:55 +0300

    GameWithBot
    Final

---
+++ NewInput/src/Game.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void playGame(Scanner scanner) {
+        int botNumber = (int) (Math.random() * 100);
+        System.out.println("Я загадую число...");
+        System.out.println("Я число загадав, тепер ти маєш вгадати");
+        System.out.println("Це число бота, щоб показав, що ок все): " + botNumber);
+        boolean game = true;
+
+        while (game) {
+            int meNumber = scanner.nextInt();
+
+            if (meNumber != botNumber) {
+                System.out.println("Не вірно! Продовжуй вгадувати!");
+            } else {
+                System.out.println("Ти вгадав!");
+                game = false;
+            }
+        }
+    }
+
+    public static void exit() {
+        System.exit(0);
+    }
+
+}
--- NewInput/src/Main.java
+++ NewInput/src/Main.java
@@ -9,34 +9,14 @@ public class Main {
         int answer = scanner.nextInt();
         switch (answer){
             case 1:
-                playGame(scanner);
+                Game.playGame(scanner);
                 break;
             case 2:
-                Exit();
+                Game.exit();
                 break;
         }
 
     }
 
-    public static void playGame(Scanner scanner) {
-        int botNumber = (int) (Math.random() * 100);
-        System.out.println("Я загадую число...");
-        System.out.println("Я число загадав, тепер ти маєш вгадати");
-        System.out.println("Це число бота, щоб показав, що ок все): " + botNumber);
-        boolean game = true;
 
-        while (game) {
-            int meNumber = scanner.nextInt();
-
-            if (meNumber != botNumber) {
-                System.out.println("Не вірно! Продовжуй вгадувати!");
-            } else {
-                System.out.println("Ти вгадав!");
-                game = false;
-            }
-        }
-    }
-    public static void Exit() {
-        return;
-    }
 }
